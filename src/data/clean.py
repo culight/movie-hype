f18996dbe0147444dd0c3e9d6fd6ca14c64830c3
culight <dmoton3.14@gmail.com>
2021-07-16 17:11:14 -0400

    update database"

--- src/data/clean.py
+++ src/data/clean.py
@@ -3,88 +3,105 @@ import os
 from datetime import datetime as dt
 import json
 
+
 def clean_imdb_data(data_dir):
-    read_file = os.path.join(data_dir, 'interim/tmdb_data.csv')
-    write_file = os.path.join(data_dir,'processed/tmdb_data.csv')
+    read_file = os.path.join(data_dir, "interim/tmdb_data.csv")
+    write_file = os.path.join(data_dir, "processed/tmdb_data.csv")
 
     if not os.path.exists(read_file):
-        print('csv file not found')
+        print("csv file not found")
         return
 
     headers = []
     cleaned_data = []
     cleaned_names = []
 
-    with open(read_file, 'r') as f:
+    with open(read_file, "r") as f:
         reader = csv.DictReader(f)
-        headers = reader.next().keys()
-        headers.remove('release_date')
-        headers.append('title_year')
+        headers = reader.fieldnames
+        headers.remove("release_date")
+        headers.append("title_year")
         for row in reader:
             # clean the movie name
-            cleaned_name = ''.join(e for e in row['movie_title'] if e.isalnum() or e.isspace()).lower()
-            row.pop('movie_title')
+            cleaned_name = "".join(
+                e for e in row["movie_title"] if e.isalnum() or e.isspace()
+            ).lower()
+            row.pop("movie_title")
             if cleaned_name not in cleaned_names:
                 cleaned_names.append(cleaned_name)
-                row['movie_title'] = cleaned_name
+                row["movie_title"] = cleaned_name
             else:
                 # this movie name already exists... skip
                 continue
 
             # convert date to year
-            if(row['release_date']):
-                row['title_year'] = dt.strptime(row['release_date'], "%m/%d/%y").year
-                row.pop('release_date')
+            if row["release_date"]:
+                row["title_year"] = dt.strptime(row["release_date"], "%m/%d/%y").year
+                row.pop("release_date")
             else:
                 continue
 
             # extract genres from json string
-            if(row['genres']):
-                genre_json = json.loads(row['genres'])
-                genres = '|'.join([g['name'] for g in genre_json])
-                row['genres'] = genres
+            if row["genres"]:
+                genre_json = json.loads(row["genres"])
+                genres = "|".join([g["name"] for g in genre_json])
+                row["genres"] = genres
             else:
                 continue
 
             # remove entries with missing financial data
-            if(row['budget'] == '0' or row['revenue'] == '0'):
+            if row["budget"] == "0" or row["revenue"] == "0":
                 continue
 
             # add the updated row
             cleaned_data.append(row)
 
-    with open(write_file, 'wb') as w:
+    with open(write_file, "wb") as w:
         writer = csv.DictWriter(w, headers)
         writer.writeheader()
         for row in cleaned_data:
             writer.writerow(row)
 
+
 def clean_additional_data(data_dir):
-    read_file = os.path.join(data_dir, 'interim/additional_data.csv')
-    write_file = os.path.join(data_dir,'processed/additional_data.csv')
+    read_file = os.path.join(data_dir, "interim/additional_data.csv")
+    write_file = os.path.join(data_dir, "processed/additional_data.csv")
 
     if not os.path.exists(read_file):
-        print('csv file not found')
+        print("csv file not found")
         return
 
     cleaned_data = []
     cleaned_names = []
 
-    with open(read_file, 'r') as r:
+    with open(read_file, "r") as r:
         reader = csv.DictReader(r)
         for row in reader:
-            name = row['title']
-            row.pop('title')
+            name = row["title"]
+            row.pop("title")
             short_name = name[0:-7]
-            cleaned_name = ''.join(e for e in short_name if e.isalnum() or e.isspace()).lower()
+            cleaned_name = "".join(
+                e for e in short_name if e.isalnum() or e.isspace()
+            ).lower()
             if cleaned_name not in cleaned_names:
                 cleaned_names.append(cleaned_name)
-                row['movie_title'] = cleaned_name
+                row["movie_title"] = cleaned_name
                 cleaned_data.append(row)
 
-    with open(write_file, 'wb') as w:
-        writer = csv.DictWriter(w, ['movie_title','imdbRating','ratingCount', \
-        'year','nrOfWins','nrOfNominations','nrOfNewsArticles','nrOfUserReviews'])
+    with open(write_file, "wb") as w:
+        writer = csv.DictWriter(
+            w,
+            [
+                "movie_title",
+                "imdbRating",
+                "ratingCount",
+                "year",
+                "nrOfWins",
+                "nrOfNominations",
+                "nrOfNewsArticles",
+                "nrOfUserReviews",
+            ],
+        )
         writer.writeheader()
         for row in cleaned_data:
             writer.writerow(row)
--- src/data/database.py
+++ src/data/database.py
@@ -2,108 +2,73 @@ import sqlite3
 import csv
 import os
 
-DB_FILE = ''
+DB_FILE = ""
+
 
 def create_file(filepath):
     global DB_FILE
     DB_FILE = filepath
 
+
 def create_tables(connection):
-    print('creating tables...')
+    print("creating tables...")
     cursor = connection.cursor()
-    finance_table = """
-        CREATE TABLE finance (
-            name  TEXT PRIMARY KEY NOT NULL,
-            year INTEGER,
-            budget INTEGER,
-            domestic_bo INTEGER,
-            international_bo INTEGER,
-            genre TEXT,
-            sequel INTEGER,
-            duration INTEGER
-        );
-    """
 
-    feedback_table = """
-        CREATE TABLE feedback (
-            name  TEXT PRIMARY KEY NOT NULL,
-            interest REAL NOT NULL,
-            FOREIGN KEY (name) REFERENCES finance(name)
+    movie_hype_table = """
+        CREATE TABLE movie_hype (
+            id TEXT,
+            genres TEXT,
+            revenue REAL,
+            popularity INTEGER,
+            budget REAL,
+            vote_count INTEGER,
+            movie_title TEXT NOT NULL,
+            vote_average REAL,
+            runtime REAL,
+            title_year TEXT NOT NULL,
+            interest_score REAL,
+            rating REAL,
+            total_votes INTEGER,
+            profit_margin REAL,
+            PRIMARY KEY (movie_title, title_year)
         );
     """
-
-    ratings_table = """
-        CREATE TABLE ratings (
-            name  TEXT PRIMARY KEY NOT NULL,
-            imdbRating REAL NOT NULL,
-            ratingCount INTEGER,
-            year INTEGER,
-            nrOfWins INTEGER,
-            nrOfNominations INTEGER,
-            nrOfNewsArticles INTEGER,
-            nrOfUserReviews INTEGER,
-            FOREIGN KEY (name) REFERENCES finance(name)
-        );
-    """
-    cursor.execute(finance_table)
-    cursor.execute(feedback_table)
-    cursor.execute(ratings_table)
+    cursor.execute(movie_hype_table)
     connection.commit()
-    print('done!')
+    print("done!")
 
-def insert_finance_data(input_file, connection):
+
+def insert_movie_hype_data(input_file, connection):
     cursor = connection.cursor()
     print("inserting ", input_file, "into", DB_FILE, "...")
     records = []
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         reader = csv.reader(f)
         column = next(reader)
         records = [record for record in reader]
-        cursor.executemany("INSERT INTO finance (name, year, budget, domestic_bo, \
-            international_bo, genre, sequel, duration) \
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)", records)
-        print('done!')
+        cursor.executemany(
+            """
+            INSERT INTO movie_hype ('id', 'genres', 'revenue', 'popularity', 'budget',
+            'vote_count', 'movie_title', 'vote_average', 'runtime', 'title_year', 'interest_score',
+            'rating', 'total_votes', 'profit_margin') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            records,
+        )
+        print("done!")
     connection.commit()
 
-def insert_feedback(input_file, connection):
-    cursor = connection.cursor()
-    #print("inserting ", input_file, "into", DB_FILE, "...")
-    records = []
-    with open(input_file, 'r') as f:
-        reader = csv.reader(f)
-        column = next(reader)
-        records = [record for record in reader]
-        cursor.executemany("INSERT INTO feedback (name, interest)\
-          VALUES (?, ?)", records)
-        print('done!')
-    connection.commit()
-
-def insert_ratings_data(input_file, connection):
-    cursor = connection.cursor()
-    print("inserting ", input_file, "into", DB_FILE, "...")
-    records = []
-    with open(input_file, 'r') as f:
-        reader = csv.reader(f)
-        column = next(reader)
-        records = [record for record in reader]
-        cursor.executemany("INSERT INTO ratings ('name','imdbRating','ratingCount', \
-        'year','nrOfWins','nrOfNominations','nrOfNewsArticles','nrOfUserReviews') \
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)", records)
-        print('done!')
-    connection.commit()
 
 def run_query(query, connection):
     cursor = connection.cursor()
-    print('executing query...')
+    print("executing query...")
     results = cursor.execute(query)
     connection.commit()
-    print('done!')
+    print("done!")
     return results
+
 
 def clear_tables(connection):
     cursor = connection.cursor()
-    print('clearing tables...')
-    cursor.execute('drop table if exists finance')
-    cursor.execute('drop table if exists feedback')
-    cursor.execute('drop table if exists ratings')
-    print('done!')
+    print("clearing tables...")
+    cursor.execute("drop table if exists movie_hype")
+    print("done!")
